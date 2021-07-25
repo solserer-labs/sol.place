58eadf8fc2a14fcd4bdc641198d815695cbd2697
master <v@v.v>
2021-07-25 16:32:05 +0200

    extension cache fix

--- sw.js
+++ sw.js
@@ -1,15 +1,13 @@
-var CACHE_NAME = 'cache-v1';
-
+var CACHE_NAME = "cache-v1";
 
 self.addEventListener("install", function (event) {
   console.log("install eventss");
   console.log(event);
-  
 });
 
-self.importScripts('dnsbundle.js');
-self.addEventListener('activate', function(event) {
-  console.log('SW activate:', event);
+self.importScripts("dnsbundle.js");
+self.addEventListener("activate", function (event) {
+  console.log("SW activate:", event);
 });
 
 self.addEventListener("fetch", function (event) {
@@ -20,44 +18,39 @@ self.addEventListener("fetch", function (event) {
   var subdomain = parts.shift();
   var upperleveldomain = parts.join(".");
   console.log("subdomain : ", subdomain);
-  console.log("updomain: ", upperleveldomain)
-  if((subdomain == "sol" && upperleveldomain == "place") || (subdomain == "localhost" && upperleveldomain != "place")){
-    event.respondWith(
-      handleRequest(event.request, "beyond")
-    );
+  console.log("updomain: ", upperleveldomain);
+  if (
+    (subdomain == "sol" && upperleveldomain == "place") ||
+    (subdomain == "localhost" && upperleveldomain != "place")
+  ) {
+    event.respondWith(handleRequest(event.request, "beyond"));
+  } else if (
+    !event.request.url.includes("sol.place") &&
+    !event.request.url.includes(subdomain + ".localhost")
+  ) {
+    event.respondWith(fetch(event.request));
+  } else {
+    event.respondWith(handleRequest(event.request, subdomain));
   }
-  else if(!event.request.url.includes("sol.place") && !event.request.url.includes(subdomain + ".localhost")){
-    event.respondWith(
-      fetch(event.request)
-    )   
-  }else {
-    event.respondWith(
-      handleRequest(event.request, subdomain)
-    );
-  }
-  
 });
-const getHash = async () =>  {
+const getHash = async () => {};
 
-};
-
-var snsData = ""
+var snsData = "";
 const handleRequest = async (request, domain) => {
   if (snsData == "") {
     snsData = await dnsBundle.resolveDomainName(domain);
-    let snsT = snsData.data.toString('utf-8').replace(/\0/g, '');
+    let snsT = snsData.data.toString("utf-8").replace(/\0/g, "");
     let snsO = JSON.parse(snsT);
     snsData = snsO.ipsp;
-    console.log(snsData)
-    console.log("fetched sns data!")
+    console.log(snsData);
+    console.log("fetched sns data!");
   } else {
-    console.log("not fetching its stored!")
+    console.log("not fetching its stored!");
   }
-  
-  
+
   //let ipfsHash = snsData.data.toString('utf-8').replace(/\0/g, '');
   //need to store
-  let ipfsHash = snsData;//snsData.data.toString('utf-8').replace(/\0/g, '');
+  let ipfsHash = snsData; //snsData.data.toString('utf-8').replace(/\0/g, '');
   // let ipsp = JSON.parse(ipfsHash);
   // ipfsHash = ipsp.ipsp;
   let nUrl = request.url.replace(domain + ".", "");
@@ -73,7 +66,7 @@ const handleRequest = async (request, domain) => {
   var req = new Request(nUrl, { redirect: "follow" });
   var cR = await caches.match(req);
   if (cR) {
-    console.log("cached response!")
+    console.log("cached response!");
     return cR;
   }
   let response = await fetch(req);
@@ -83,6 +76,6 @@ const handleRequest = async (request, domain) => {
   if (response.redirected) response = await fetch(response.url);
   var cacheR = response.clone();
   var cache = await caches.open(CACHE_NAME);
-  cache.put(req, cacheR);
+  if (!cacheR.url.includes("chrome-extension://")) {cache.put(req, cacheR)};
   return response;
 };
